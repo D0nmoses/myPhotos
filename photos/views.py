9f038b820071f107042cd7d441e2f3d0a7f6dccd
Don Moses <githinjidon@gmail.com>
2021-10-13 11:43:28 +0300

    add images context in index view

--- photos/views.py
+++ photos/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 def index(request):
     
-    return render(request, 'all-pics/index.html')
+    images = Image.all_images()
+    return render(request, 'all-pics/index.html', {"images":images})
 
 
 def search_results(request):
